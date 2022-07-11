6c0a28166c6031700994e67ce0ad60523f118d57
MohamedasiqS <98573822+MohamedasiqS@users.noreply.github.com>
2022-07-11 18:43:25 +0530

    Delete 006_display the smallest word from the string.py

--- 11_07_2022/006_display the smallest word from the string.py
+++
@@ -1,8 +0,0 @@
-#display the smallest word from the string
-str = input ("Enter a sentence\n")
-sub_str = str.split()
-small = len(sub_str[0])
-for i in sub_str:
-    if small > len(i):
-        sm_str = i
-print (sm_str)
